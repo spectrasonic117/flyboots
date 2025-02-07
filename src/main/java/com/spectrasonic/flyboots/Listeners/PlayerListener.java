10ca6bd83e3d7cc57f422e436d205bfa0239fa53
Manuel Heredia <Spectrasonic117@gmail.com>
2025-02-07 10:51:35 -0600

    🩹 - 1.0.4 fix fly mechanic in aventure and survival

--- src/main/java/com/spectrasonic/flyboots/Listeners/PlayerListener.java
+++ src/main/java/com/spectrasonic/flyboots/Listeners/PlayerListener.java
@@ -1,26 +1,49 @@
 package com.spectrasonic.flyboots.Listeners;
 
 import com.spectrasonic.flyboots.Utils.ItemBuilder;
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerToggleFlightEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class PlayerListener implements Listener {
 
     @EventHandler
+    public void onPlayerMove(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+        GameMode gameMode = player.getGameMode();
+        
+        if (gameMode != GameMode.SURVIVAL && gameMode != GameMode.ADVENTURE) {
+            return;
+        }
+
+        ItemStack boots = player.getInventory().getBoots();
+        
+        if (boots != null && new ItemBuilder().isFlyBoots(boots)) {
+            player.setAllowFlight(true);
+        } else {
+            player.setAllowFlight(false);
+            player.setFlying(false);
+        }
+    }
+
+    @EventHandler
     public void onDoubleJump(PlayerToggleFlightEvent event) {
         Player player = event.getPlayer();
+        GameMode gameMode = player.getGameMode();
+        
+        if (gameMode != GameMode.SURVIVAL && gameMode != GameMode.ADVENTURE) {
+            return;
+        }
+
         ItemStack boots = player.getInventory().getBoots();
 
         if (boots != null && new ItemBuilder().isFlyBoots(boots)) {
             event.setCancelled(true);
-            player.setAllowFlight(false);
-            player.setFlying(false);
-            player.setVelocity(player.getLocation().getDirection().multiply(1.5).setY(1));
-        } else {
-            player.setAllowFlight(false);
+            player.setFlying(!player.isFlying());
         }
     }
 }
